d0433297e4e7f2d0447b295103c61e75f782e4ed
Dimitry Loutsenko <333333y@gmail.com>
2016-10-12 18:17:02 +0530

    скрол умеет работать со ссылками типа http://web-site/index.html/#disqus_thread

--- scroll-to/scroll-to-N-mobile.js
+++ scroll-to/scroll-to-N-mobile.js
@@ -5,14 +5,25 @@
         this._listenedBlock = options.listenedBlock || document.body;
         this._translationElementSelector = options.translation || false;
     }
+
     ScrollToAnchor.prototype.init = function () {
-        this._listenedBlock.addEventListener('click', this.anchorClickListener.bind(this));
+        $(this._listenedBlock).on('click', this.anchorClickListener.bind(this));
     };
     ScrollToAnchor.prototype.anchorClickListener = function (e) {
         var elem = e.target;
-        var anchorWithHash = elem.closest('a[href^="#"]:not([data-scroll="disable"])');
+        var anchor = elem.closest('a[href*="#"]:not([data-scroll="disable"])');
 
-        if (!anchorWithHash || !anchorWithHash.hash.length) return;
+        if (!anchor) return;
+
+        var anchorWithHash = anchor.closest('a[href^="#"]');
+        var windowPath = window.location.origin + window.location.pathname;
+        var anchorPath = anchor.href.slice(0, anchor.href.indexOf('#'));
+
+        if (windowPath === anchorPath) {
+            anchorWithHash = anchor;
+        }
+
+        if (!anchorWithHash || anchorWithHash.hash.length < 2) return;
 
         e.preventDefault();
 
@@ -25,13 +36,14 @@
             translation = document.querySelector(this._translationElementSelector).offsetHeight;
         }
 
-        if(! document.querySelector(target)) return;
+        if (!document.querySelector(target)) return;
 
         this.smoothScroll(target, translation);
     };
     ScrollToAnchor.prototype.smoothScroll = function (selector, translation) {
         $("html, body").animate({
-                scrollTop: $(selector).offset().top - (translation||0)},
+                scrollTop: $(selector).offset().top - (translation || 0)
+            },
             500
         );
     };
@@ -89,4 +101,55 @@
 
         }
     })();
+})();
+
+/*ScrollToAnchor unstable*/
+(function() {
+    /*ScrollToAnchor class*/
+    function ScrollToAnchor(options) {
+        this._listenedBlock = options.listenedBlock || document.body;
+        this._translationElementSelector = options.translation || false;
+    }
+
+    ScrollToAnchor.prototype.init = function () {
+        $(this._listenedBlock).on('click', this.anchorClickListener.bind(this));
+    };
+    ScrollToAnchor.prototype.anchorClickListener = function (e) {
+        var elem = e.target;
+        var anchor = elem.closest('a[href*="#"]:not([data-scroll="disable"])');
+
+        if (!anchor) return;
+
+        var anchorWithHash = anchor.closest('a[href^="#"]');
+        var windowPath = window.location.origin + window.location.pathname;
+        var anchorPath = anchor.href.slice(0, anchor.href.indexOf('#'));
+
+        if (windowPath === anchorPath) {
+            anchorWithHash = anchor;
+        }
+
+        if (!anchorWithHash || anchorWithHash.hash.length < 2) return;
+
+        e.preventDefault();
+
+        var target = anchorWithHash.hash;
+        var translation = 0;
+
+        if (anchorWithHash.hasAttribute('data-translation')) {
+            translation = anchorWithHash.getAttribute('data-translation');
+        } else if (this._translationElementSelector) {
+            translation = document.querySelector(this._translationElementSelector).offsetHeight;
+        }
+
+        if (!document.querySelector(target)) return;
+
+        this.smoothScroll(target, translation);
+    };
+    ScrollToAnchor.prototype.smoothScroll = function (selector, translation) {
+        $("html, body").animate({
+                scrollTop: $(selector).offset().top - (translation || 0)
+            },
+            500
+        );
+    };
 })();
