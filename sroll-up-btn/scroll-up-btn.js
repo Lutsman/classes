14f1ccc8e0b296711871a97401b7dbf5d13da5a3
Dimitry Loutsenko <333333y@gmail.com>
2016-11-27 03:58:40 +0530

    сделал конструктор для кнопки скрола вверх

--- sroll-up-btn/scroll-up-btn.js
+++ sroll-up-btn/scroll-up-btn.js
@@ -1,28 +1,34 @@
 /*ScrollUp button*/
 (function(){
-    var buttonUp = '<div id="scrollUp"><i class="upButton"></i></div>';
-    var flag = false;
-
-    $('body').append($(buttonUp));
-
-
-    $('#scrollUp').click( function(){
+    function ScrollTop() {
+        this._$btn = $('<div id="scrollUp"><i class="upButton"></i></div>');
+        this._isActive = false;
+        
+        this.init();
+    }
+    ScrollTop.prototype.init = function () {
+        $('body').append(this._$btn);
+        
+        this.scrollBtnToggler();
+        
+        this._$btn.on('click', this.scrollTop.bind(this));
+        $(window).on('scroll', this.scrollBtnToggler.bind(this));
+    };
+    ScrollTop.prototype.scrollBtnToggler = function () {
+        if ( $(document).scrollTop() > $(window).height() && !this._isActive ) {
+            this._$btn.fadeIn({queue : false, duration: 400})
+                .animate({'bottom' : '40px'}, 400);
+            this._isActive = true;
+        } else if ( $(document).scrollTop() < $(window).height() && this._isActive ) {
+            this._$btn.fadeOut({queue : false, duration: 400})
+                .animate({'bottom' : '-20px'}, 400);
+            this._isActive = false;
+        }
+    };
+    ScrollTop.prototype.scrollTop = function(){
         $("html, body").animate({scrollTop: 0}, 500);
         return false;
-    });
-
-    $(window).scroll(scrollBtnToggler);
-    scrollBtnToggler();
-
-    function scrollBtnToggler() {
-        if ( $(document).scrollTop() > $(window).height() && !flag ) {
-            $('#scrollUp').fadeIn({queue : false, duration: 400});
-            $('#scrollUp').animate({'bottom' : '40px'}, 400);
-            flag = true;
-        } else if ( $(document).scrollTop() < $(window).height() && flag ) {
-            $('#scrollUp').fadeOut({queue : false, duration: 400});
-            $('#scrollUp').animate({'bottom' : '-20px'}, 400);
-            flag = false;
-        }
-    }
+    };
+    
+    var scrollTopBtn = new ScrollTop();
 })();
