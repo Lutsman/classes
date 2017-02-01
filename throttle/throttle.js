a7a98112dbbd1b51befdadc79193dc36841eaa46
Lutsman <333333y@gmail.com>
2017-02-02 00:00:44 +0530

    добавление функции debounce, экспорт функций в jquery или window

--- throttle/throttle.js
+++ throttle/throttle.js
@@ -1,29 +1,58 @@
-function throttle(func, ms) {
+(function (window) {
+    'use strict';
 
-    var isThrottled = false,
-        savedArgs,
-        savedThis;
+    var $ = window.jquery || window;
 
-    function wrapper() {
+    /*функция обертка выполняет вызов func и ждет ms милисекунд
+     * если во время ожидания происходят вызовы функции игнорирует*/
+    $.debounce = function (func, ms) {
+        var state = false;
 
-        if (isThrottled) { // (2)
-            savedArgs = arguments;
-            savedThis = this;
-            return;
+        function wrapper() {
+            if (state) return;
+
+            func.apply(this, arguments);
+            state = true;
+
+            setTimeout(function () {
+                state = false;
+            }, ms);
         }
 
-        func.apply(this, arguments); // (1)
-
-        isThrottled = true;
-
-        setTimeout(function() {
-            isThrottled = false; // (3)
-            if (savedArgs) {
-                wrapper.apply(savedThis, savedArgs);
-                savedArgs = savedThis = null;
-            }
-        }, ms);
+        return wrapper;
     }
 
-    return wrapper;
-}
+    /*функция обертка выполняет вызов func и ждет ms милисекунд
+    * если во время ожидания происходят вызовы функции запоминает последний
+    * и выполняет в конце ожидания*/
+    $.throttle = function (func, ms) {
+
+        var isThrottled = false,
+            savedArgs,
+            savedThis;
+
+        function wrapper() {
+
+            if (isThrottled) { // (2)
+                savedArgs = arguments;
+                savedThis = this;
+                return;
+            }
+
+            func.apply(this, arguments); // (1)
+
+            isThrottled = true;
+
+            setTimeout(function() {
+                isThrottled = false; // (3)
+                if (savedArgs) {
+                    wrapper.apply(savedThis, savedArgs);
+                    savedArgs = savedThis = null;
+                }
+            }, ms);
+        }
+
+        return wrapper;
+    };
+
+})(this);
